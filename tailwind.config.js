3fe0b21b01f586552f8dfbe0858281bc542eb61a
izik-adio <adioisaac24@gmail.com>
2024-04-02 23:17:03 +0100

    Refactor: Update CSS and design elements
    - Replaced `#deepBrown` color with `#dBrown`
    - Replaced `#lightBrown` color with `#lBrown`
    - Replaced `#background` color with `#F0F0F2`
    - Added `#foreground` and `#highlight` colors
    - Updated font sizes and styles
    - Adjusted hero height and position on smaller screens
    - Removed unnecessary media queries
    - Added `whitespace-nowrap` class to prevent line breaks in headings
    - Added `text-2xl`, `text-3xl`, and `text-5xl` classes for larger text sizes
    - Updated anchor tags in hero heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.{html, js}"],
   theme: {
     extend: {
       colors: {
-        deepBrown: "#1f1b29",
-        lightBrown: "#77595b",
-        background: "#e6e6eb",
+        dBrown: "#1C1A26", // Deep Brown (Text - Headings, Buttons, CTAs)
+        lBrown: "#58413D", // Lighter Brown (Text - Secondary, Highlights)
+        background: "#F0F0F2", // White (Background - Main Website Area)
+        foreground: "#e6e6eb", // Off-White (Content Areas, Product Boxes)
+        highlight: "#A9BF04", // Bright Green (CTAs, Special Offers)
       },
       fontFamily: {
         actor: ["Actor", "sans-serif"],
